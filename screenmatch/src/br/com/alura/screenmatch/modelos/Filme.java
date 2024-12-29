b12d5f242be0b5ba9f9b22cc57766cfa13271878
Rennan Ventura <rennan.ventura.alves@gmail.com>
2024-12-29 00:13:21 -0300

    ajustando diretório

--- screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,60 +1,29 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+import br.com.alura.screenmatch.calculos.Classificavel;
 
-    public int getTotalDeAvaliacoes() {
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
 
-        return totalDeAvaliacoes;
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
     }
 
-    public String getNome() {
-        return nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
+    @Override
+    public int getClassificacao() {
+        return (int) pegaMedia() / 2;
     }
 
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void exibeFilme(){
-        System.out.println("nome do filme: " + nome);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes ++;
-    }
-
-    public double media(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
 }
